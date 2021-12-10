418ed3ba8a97eccadc776d1a21f21ed10da1ba24
nsabi <Albiate83!>
2021-12-10 10:02:50 +0100

    svg icona

--- src/components/BoxDettaglio.js
+++ src/components/BoxDettaglio.js
@@ -6,7 +6,7 @@ export const BoxDettaglio = ({ clearDettaglio, boxz }) => {
   return (
     <div className="ps-content__dettaglio" id="boxdettaglio">
       <div className="ps-content__dettaglio__content">
-        <a onClick={clearDettaglio} className="ps-content__dettaglio__close">X</a>
+        <a onClick={clearDettaglio} className="ps-content__dettaglio__close"></a>
         <div className="ps-content__dettaglio__title">{boxz.title}</div>
         <div className="ps-content__dettaglio__description">{boxz.description}</div>
       </div>
